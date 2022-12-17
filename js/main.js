c7606fe1174aa5a470f494a12b119b6937557162
crpachecov <crpacheco@unicesar.edu.co>
2022-12-17 10:23:58 -0500

    links in progress

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const hamburguerIcon = document.querySelector('.nav__hamburguer');
 const navOverlay = document.querySelector('.nav__overlay');
+const navLink = document.querySelector('.nav__menu-list');
 
 hamburguerIcon.addEventListener('click', () => {
     hamburguerIcon.classList.toggle('nav__hamburguer--open');
@@ -11,4 +12,10 @@ window.addEventListener('scroll', () => {
     let nav = document.querySelector('.nav');
 
     nav.classList.toggle("nav--scrolled", window.scrollY > 0);
+});
+
+navLink.addEventListener('click', () => {
+    hamburguerIcon.classList.toggle('nav__hamburguer--open');
+
+    navOverlay.classList.toggle('nav__overlay--show');
 });
